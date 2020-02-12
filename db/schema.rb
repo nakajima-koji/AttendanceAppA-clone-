c51142436282ebbd2da3a632ca4a6f7a2703bfd5
nakajima-koji <crazy.max.meteora@gmail.com>
2020-02-12 13:22:22 +0000

    git

--- db/schema.rb
+++ db/schema.rb
@@ -31,8 +31,8 @@ ActiveRecord::Schema.define(version: 20191220052439) do
     t.string "password_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2019-12-19 23:00:00"
-    t.datetime "work_time", default: "2019-12-19 22:30:00"
+    t.datetime "basic_time", default: "2020-02-11 23:00:00"
+    t.datetime "work_time", default: "2020-02-11 22:30:00"
     t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
